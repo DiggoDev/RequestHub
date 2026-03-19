543b5c33c5e337be5a12384c629219f673c31548
DiggoDev <diggo16@gmail.com>
2026-03-19 14:35:26 +0100

    Set up docker server and database

---
+++ server/src/db/index.ts
@@ -0,0 +1,40 @@
+import { Client } from 'pg'
+
+export async function initializeDb() {
+  await makeDbAction(async (client: Client) => {
+    await client.query(`  
+      CREATE TABLE IF NOT EXISTS users (id int, username text, password text);
+      `);
+  })
+}
+
+export async function insertUser(username: string, password: string) {
+  await makeDbAction(async (client: Client) => {
+    await client.query(`  
+      INSERT INTO users(id, username, password) VALUES (1, $1, $2);
+      `, [username, password]);
+  })
+}
+
+export async function getUserByUsername(username: string) {
+  return await makeDbAction<{ username: string, password: string }>(async (client: Client) => {
+    const result = await client.query(`  
+      SELECT * FROM users WHERE username=$1;
+      `, [username])
+      return result.rows[0]
+  })
+}
+
+async function makeDbAction<T>(action: (client: Client) => Promise<T>) {
+  const c = await new Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST
+  }).connect()
+  try {
+    return await action(c)
+  } finally {
+    c.end()
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,23 +1,52 @@
 import Fastify from "fastify";
 import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config()
+dotenv.config({ path: path.resolve('../', '.env') })
+
+import { initializeDb, insertUser, getUserByUsername } from "./db";
 
 const fastify = Fastify({
   logger: true
 })
+
+initializeDb().then(() => console.log('Database initialized'))
 
 // Declare a route
 fastify.get('/', function (request, reply) {
   reply.send({ hello: 'world' })
 })
 
+fastify.get('/test', async(request, reply) => {
+  const username = 'user1410'
+  const password = 'pass'
+
+  await insertUser(username, password)
+
+  const dbUser = await getUserByUsername(username)
+
+  return reply.send(dbUser)
+
+})
+
+fastify.post('/authenticate', (request, reply) => {
+  const { username, password }: { username: string; password: string  } = JSON.parse(request.body as string)
+
+  if (username === 'admin' && password === 'password') {
+    return reply.send({ token: 'admin-token' })
+  }
+  return reply.code(401).send({ error: 'Invalid credentials' })
+  
+})
+
+console.log(process.env.SERVER_PORT)
+
 // Run the server!
-const port = process.env.PORT ? Number(process.env.PORT) : 3000
-fastify.listen({ port }, function (err, address) {
+const port = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000
+const host = "0.0.0.0"
+fastify.listen({ port, host }, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
-  // Server is now listening on ${address}
 })
